9641bd5ffd3da514c6b922d75c7c700587082c6a
KamranHussen71 <140729209+KamranHussen71@users.noreply.github.com>
2023-11-03 23:47:59 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,38 @@
  import 'dart:convert';
-import 'package:flutter/material.dart';
+ import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+ void main() {
+   runApp(MyApp());
+ }
 
-class WeatherInfo {
-  final String city;
-  final int temperature;
-  final String condition;
-  final int humidity;
-  final double windSpeed;
+ class WeatherInfo {
+   final String city;
+   final int temperature;
+   final String condition;
+   final int humidity;
+   final double windSpeed;
 
-  WeatherInfo({
-    required this.city,
-    required this.temperature,
-    required this.condition,
-    required this.humidity,
-    required this.windSpeed,
-  });
+   WeatherInfo({
+     required this.city,
+     required this.temperature,
+     required this.condition,
+     required this.humidity,
+     required this.windSpeed,
+   });
 
-  factory WeatherInfo.fromJson(Map<String, dynamic> json) {
-    return WeatherInfo(
-      city: json['city'],
-      temperature: json['temperature'],
-      condition: json['condition'],
-      humidity: json['humidity'],
-      windSpeed: json['windSpeed'].toDouble(),
-    );
-  }
-}
+   factory WeatherInfo.fromJson(Map<String, dynamic> json) {
+     return WeatherInfo(
+       city: json['city'],
+       temperature: json['temperature'],
+       condition: json['condition'],
+       humidity: json['humidity'],
+       windSpeed: json['windSpeed'].toDouble(),
+     );
+   }
+ }
 
-class MyApp extends StatelessWidget {
-  final String jsonStr = '''
+ class MyApp extends StatelessWidget {
+   final String jsonStr = '''
   [
     {
       "city": "New York",
@@ -72,39 +72,43 @@ class MyApp extends StatelessWidget {
   ]
   ''';
 
-  List<WeatherInfo> weatherData = [];
+   List<WeatherInfo> weatherData = [];
 
-  MyApp({super.key}) {
-    List<dynamic> jsonList = json.decode(jsonStr);
-    weatherData = jsonList.map((item) => WeatherInfo.fromJson(item)).toList();
-  }
+   MyApp({super.key}) {
+     List<dynamic> jsonList = json.decode(jsonStr);
+     weatherData = jsonList.map((item) => WeatherInfo.fromJson(item)).toList();
+   }
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Weather Info App'),
-        ),
-        body: ListView.builder(
-          itemCount: weatherData.length,
-          itemBuilder: (context, index) {
-            WeatherInfo weatherInfo = weatherData[index];
-            return ListTile(
-              title: Text('City: ${weatherInfo.city}'),
-              subtitle: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text('Temperature: ${weatherInfo.temperature}°C'),
-                  Text('Condition: ${weatherInfo.condition}'),
-                  Text('Humidity: ${weatherInfo.humidity}%'),
-                  Text('Wind Speed: ${weatherInfo.windSpeed} m/s'),
-                ],
-              ),
-            );
-          },
-        ),
-      ),
-    );
-  }
-}
+   @override
+   Widget build(BuildContext context) {
+     return MaterialApp(
+       home: Scaffold(
+         appBar: AppBar(
+           title: const Text('Weather Info App'),
+         ),
+         body: ListView.builder(
+           itemCount: weatherData.length,
+           itemBuilder: (context, index) {
+             WeatherInfo weatherInfo = weatherData[index];
+             return Card(
+               elevation: 1, // Set the elevation for the shadow effect
+               child: ListTile(
+                 title: Text('City: ${weatherInfo.city}'),
+                 subtitle: Column(
+                   crossAxisAlignment: CrossAxisAlignment.start,
+                   children: <Widget>[
+                     Text('Temperature: ${weatherInfo.temperature}°C'),
+                     Text('Condition: ${weatherInfo.condition}'),
+                     Text('Humidity: ${weatherInfo.humidity}%'),
+                     Text('Wind Speed: ${weatherInfo.windSpeed} m/s'),
+                   ],
+                 ),
+               ),
+             );
+           },
+         ),
+       ),
+     );
+   }
+ }
+
